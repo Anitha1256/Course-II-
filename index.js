bf44dfd768a03e71b4d2893c43fe928b56ca1c67
anitha <anitha3venu@gmail.com>
2025-04-22 12:20:41 +0530

    commit 2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// let numbers = [3, 4, 5];
-// numbers.unshift(15);
-// console.log(numbers);
+let numbers = [3, 4, 5];
+numbers.unshift(15);
+console.log(numbers);
 let fruit = [15, 12, 11, 3, 1, 2, 4, 5, 6, 7, 8];
-// fruit.splice(6, 5, 20);
-// console.log(fruit);
+fruit.splice(6, 5, 20);
+console.log(fruit);
 for (i = 0; i < fruit.length; i++) {
   console.log(fruit[i]);
 }
